8d476a1bcc7f10815d74ddb8cc0e80e3e1e678da
kim <kimkozlov.ks@gmail.com>
2023-05-23 15:20:29 -0600

    Added routing with lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import {FirstComponent} from "./components/FirstComponent";
+import React, { Suspense } from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/Main/MainPageAsync";
+import {AboutPageAsync} from "./pages/About/AboutPageAsync";
 
 function App() {
     return (
         <div className='app'>
-            <FirstComponent />
+            <Link to='/'>Main</Link>
+            <Link to='/about'>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPageAsync/>} />
+                    <Route path='/about' element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 }
