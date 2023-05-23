5323f101507efbc274fee8ae5fc14def382320ba
kim <kimkozlov.ks@gmail.com>
2023-05-23 14:43:56 -0600

    Setup css modules and css build options

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FirstComponent} from "./components/FirstComponent";
+import './index.scss'
+
+function App() {
+    return (
+        <div className='app'>
+            <FirstComponent />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import css from './FirstComponent.module.scss'
+
+export function FirstComponent() {
+    return (
+        <div className={css.color}>Red Text</div>
+    );
+}
